f79ef62baa7cfbf4c24b97dea817290b57b0cb39
gaziz73 <vasilka.lo.lo.lo@gmail.com>
2024-11-21 10:57:58 +0300

    Create second.java

---
+++ second.java
@@ -0,0 +1,8 @@
+public class second {
+    public static void main(String[] args) {
+           
+        System.out.println("222222222222222222222222222222");
+        System.out.println("22222222222222222");
+    }
+
+}
